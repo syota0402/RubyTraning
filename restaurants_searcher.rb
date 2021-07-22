bc2d001398e3d90ef4c5d384bfa883efda407792
EC2 Default User <ec2-user@ip-172-31-28-26.ap-northeast-1.compute.internal>
2021-07-22 14:55:09 +0000

    9章完了

--- restaurants_searcher.rb
+++ restaurants_searcher.rb
@@ -1,32 +1,39 @@
 require 'net/http'
 require 'json'
+require "csv"
 
-#初期設定
+# 初期設定
 KEYID = "0fa19af5f5d67db6"
 COUNT = 100
 PREF = "Z011"
-FREEWORD = "渋谷駅" 
+FREEWORD = "渋谷駅"
 FORMAT = "json"
-PARAMS = {"key": KEYID,"count":COUNT,"large_area":PREF,"keyword":FREEWORD,"format":FREEWORD}
+PARAMS = {"key": KEYID, "count":COUNT, "large_area":PREF, "keyword":FREEWORD, "format":FORMAT}
 
 def write_data_to_csv(params)
-    restaurants = []
+
+    restaurants = [["名称","営業日","住所","アクセス"]]
     uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/")
     uri.query = URI.encode_www_form(PARAMS)  
+
     json_res = Net::HTTP.get uri
     
+    # JSON.load(response)
     response = JSON.load(json_res)
     
     if response.has_key?("error") then
         puts "エラーが発生しました！"
     end
     for restaurant in response["results"]["shop"] do
-        rest_name = restaurant["name"]
-        restaurants.append(rest_name)
+        rest_info = [restaurant["name"], restaurant["open"], restaurant["address"], restaurant["access"]]
+        puts rest_info
+        restaurants.append(rest_info)
     end
     
-    File.open("restaurants_list.csv", "w") do |file|
-        file << restaurants
+    CSV.open("restaurants_list.csv", "w") do |csv|
+        restaurants.each do |rest_info|
+            csv << rest_info
+        end
     end
     return puts restaurants
 end
